fdaab939a1840c0fda45680058a3e2d492d1afcf
Sylvain Desvé <sylvain.desve@gmail.com>
2023-05-12 21:06:45 +0200

    add MongoDB event store implementation

---
+++ get-it-done-backend/src/shared/infrastructure/domain/event/MongoEventStore.ts
@@ -0,0 +1,38 @@
+import { Collection } from "mongodb";
+
+import { DomainMessage } from "../../../domain/event/DomainMessage";
+import { EventStore } from "../../../domain/event/EventStore";
+
+/**
+ * MongoDB implementation of an event store
+ */
+export class MongoEventStore implements EventStore {
+  /**
+   *
+   * @param mongoCollection The collection in where events are stored
+   */
+  constructor(protected mongoCollection: Collection<DomainMessage>) {}
+
+  /**
+   * Load all event messages related to the aggregate root with provided ID
+   * @param id Aggregate root ID
+   * @returns The event messages
+   */
+  async load(id: string) {
+    const cursor = this.mongoCollection.find(
+      { id },
+      { projection: { _id: 0 } }
+    );
+    const events = await cursor.toArray();
+    return events;
+  }
+
+  /**
+   * Append event messages for an aggregate root to the store
+   * @param id Aggregate root ID
+   * @param events Event messages to append to the store
+   */
+  async append(id: string, events: DomainMessage[]) {
+    await this.mongoCollection.insertMany(events, { ordered: true });
+  }
+}
