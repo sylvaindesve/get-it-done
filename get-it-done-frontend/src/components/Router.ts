2c766a983ed906ae5ccdc857703db3d129aa3623
Sylvain Desvé <sylvain.desve@gmail.com>
2023-05-13 21:14:39 +0200

    add doc to Router

--- get-it-done-frontend/src/components/Router.ts
+++ get-it-done-frontend/src/components/Router.ts
@@ -4,21 +4,40 @@ interface Route {
 
 type RouteListener = (route: Route, routerElement: Router) => void;
 
+/**
+ * A router component.
+ * All child elements with a `route` attribute will trigger a navigation to
+ * the `route` attribute value when clicked. Navigation will actually occurs
+ * if there is a corresponding route listener attached to the router.
+ */
 export class Router extends HTMLElement {
   private routeListeners: { [path: string]: RouteListener } = {};
 
+  // eslint-disable-next-line jsdoc/require-jsdoc
   constructor() {
     super();
     this.handleClick = this.handleClick.bind(this);
     this.handlePopstate = this.handlePopstate.bind(this);
   }
 
+  // eslint-disable-next-line jsdoc/require-jsdoc
   connectedCallback() {
     this.navigate(window.location.pathname);
     this.addEventListener("click", this.handleClick);
     window.addEventListener("popstate", this.handlePopstate);
   }
 
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  disconnectedCallback() {
+    this.removeEventListener("click", this.handleClick);
+    window.removeEventListener("popstate", this.handlePopstate);
+  }
+
+  /**
+   * Register a route listener to this router.
+   * @param path The path to listen to
+   * @param listener The listener to call
+   */
   addRouteListener(path: string, listener: RouteListener) {
     if (path in this.routeListeners) {
       throw `Path '${path}' already registered`;
@@ -27,6 +46,10 @@ export class Router extends HTMLElement {
     }
   }
 
+  /**
+   * Navigate to the povided path if there is a corresponding route listener
+   * @param path The patch to navigate to
+   */
   navigate(path: string) {
     if (path in this.routeListeners) {
       window.history.pushState(null, "", path);
@@ -34,6 +57,10 @@ export class Router extends HTMLElement {
     }
   }
 
+  /**
+   * Handles click events so that it can trigger navigation if appropriate
+   * @param event Click event
+   */
   private handleClick(event: MouseEvent) {
     const clicked = event.target;
     if (
@@ -47,13 +74,11 @@ export class Router extends HTMLElement {
     }
   }
 
+  /**
+   * Handles `popstate` events so that the browser `back` action still function
+   */
   private handlePopstate() {
     this.navigate(window.location.pathname);
-  }
-
-  disconnectedCallback() {
-    this.removeEventListener("click", this.handleClick);
-    window.removeEventListener("popstate", this.handlePopstate);
   }
 }
 
