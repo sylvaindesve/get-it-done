620ce2f73c16cb8e3212ca474c8da031ab1167be
Sylvain Desvé <sylvain.desve@gmail.com>
2023-05-13 21:04:48 +0200

    add tests for Router

--- get-it-done-frontend/spec/components/Router.spec.ts
+++ get-it-done-frontend/spec/components/Router.spec.ts
@@ -1,5 +1,32 @@
+import chai, { expect } from "chai";
 import { describe, it } from "mocha";
+import { fake } from "sinon";
+import sinonChai from "sinon-chai";
+
+chai.use(sinonChai);
+
+import { Router } from "../../src/components/Router";
 
 describe("Router", function () {
-  it("should have tests");
+  it("can register route listeners", function () {
+    const router = new Router();
+    expect("addRouteListener" in router).to.be.true;
+  });
+
+  it("calls the appropriate route listener when a route is clicked", function () {
+    document.body.innerHTML = `<gid-router>
+      <a route="/someroute"></a>
+    </gid-router>`;
+
+    const router = document.querySelector("gid-router") as Router;
+    const link = document.querySelector("a") as HTMLAnchorElement;
+
+    const listener = fake();
+    router.addRouteListener("/someroute", listener);
+
+    link.click();
+
+    expect(listener).to.have.been.called;
+    expect(listener.firstCall.firstArg).to.deep.equal({ path: "/someroute" });
+  });
 });
